5f41b6521627d6eabdd364ef34b8731a207e9994
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2023-09-22 09:58:55 -0500

    added meta tags

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <header>
+        <img>Logo</img>
+        <nav>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order online</a></li>
+            <li><a>Login</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
