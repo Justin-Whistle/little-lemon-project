92781b49cc9da091562fac271e07346613693269
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2023-09-30 13:26:54 -0500

    still trying to figure out the reducer

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import { useState } from 'react';
 
-export default function BookingForm() {
-    const [reservation, setReservation]=useState([
-        {
-            name:"",
-            date:"",
-            time:"",
-            guests:"",
-            occasion:""
-        }
-    ])
-    // const [availableTimes, setAvailableTimes]=useState('')
+export default function BookingForm(props) {
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState();
+    const [guests, setGuests] = useState("1");
+    const [occasion, setOccasion] = useState("Lunch");
+
+    const [finalTime, setFinalTime] = useState(
+        props.availableTimes.map(times => (
+        <option key={times}>{times}</option>
+      )));
+
+    // function handleDateChange(e) {
+    //     setDate(e.target.value);
+
+    //     var stringify = e.target.value;
+    //     const date = new Date(stringify);
+
+    //     props.updateTimes(date);
+
+    //     setFinalTime(props.availableTimes.map(times => (
+    //     <option key={times}>{times}</option>
+    //     )));
+    // }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setReservation('')
-        console.log({reservation})
+        setName("");
+        setDate("");
+        setTime("");
+        setGuests("1");
+        setOccasion("");
+        console.log({ name, date, time, guests, occasion })
     }
 
     return (
@@ -27,34 +44,29 @@ export default function BookingForm() {
                     type=""
                     required
                     id="name"
-                    value={reservation.name}
-                    onChange={e => setReservation({...reservation, name: e.target.value})}>
+                    value={name}
+                    onChange={e => setName(e.target.value)}>
                 </input>
                 <label htmlFor="date">Choose date</label>
                 <input
                     type="date"
                     id="date"
-                    value={reservation.date}
-                    onChange={e => setReservation({...reservation, date: e.target.value})}>
+                    value={date}
+                    onChange={ e => setDate(e.target.value)}>
                 </input>
                 <label htmlFor="time">Choose time</label>
-                <select id="time" onChange={e => setReservation({...reservation, time: e.target.value})}>
-                    <option value="17:00">17:00</option>
-                    <option value="18:00">18:00</option>
-                    <option value="19:00">19:00</option>
-                    <option value="20:00">20:00</option>
-                    <option value="21:00">21:00</option>
-                    <option value="22:00">22:00</option>
+                <select id="time" value={time} onChange={e => setTime(e.target.value)}>
+                    {finalTime}
                 </select>
-                <label htmlFor="guests">Party size: {reservation.guests}</label>
+                <label htmlFor="guests">Party size: {guests}</label>
                 <input
                     type="range"
-                    value={reservation.guests}
+                    value={guests}
                     placeholder="1" min="1" max="10" id="guests"
-                    onChange={e => setReservation({...reservation, guests: e.target.value})}>
+                    onChange={e => setGuests(e.target.value)}>
                 </input>
                 <label htmlFor="occasion">Occasion</label>
-                <select id="occasion" onChange={e => setReservation({...reservation, occasion: e.target.value})}>
+                <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
                     <option value="Lunch">Lunch</option>
                     <option value="Dinner">Dinner</option>
                     <option value="Birthday">Birthday</option>
@@ -62,11 +74,7 @@ export default function BookingForm() {
                 </select>
                 <input className="reserve-button" type="submit" value="Make Your reservation"></input>
             </form>
+            {/* <p>{availableTimes}</p> */}
         </div>
     )
 }
-// const [name, setName] = useState("")
-// const [date, setDate] = useState("")
-// const [time, setTime] = useState()
-// const [guests, setGuests] = useState()
-// const [occasion, setOccasion] = useState("Lunch")
--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+// import React, { useReducer } from 'react';
 import BookingForm from './BookingForm';
+import { useReducer } from 'react';
 
 export default function BookingPage() {
+  var initializeTimes = ["17:00", "18:00", "19:00", "20:00", "21:00"];
+
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+
+  function updateTimes(date) {
+    // return {availableTimes}
+  }
+
   return (
     <div>
-        <BookingForm />
+        <BookingForm availableTimes={availableTimes} updateTimes={dispatch} />
     </div>
   )
 }
